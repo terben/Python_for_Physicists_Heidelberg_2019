0fa1a4631aace89858a7fc62cc0c8b883c03b629
Thomas Erben <terben@astro.uni-bonn.de>
2019-04-01 16:54:08 +0200

    script updated for 2019 lectures

--- start-jupyter-notebook.sh
+++ start-jupyter-notebook.sh
@@ -3,14 +3,19 @@
 # script to start jupyter notebooks on the Heidelberg computer CIP-Pool.
 
 # The Heidelberg CIP-Pool offers a large amount of terminals connected
-# to alimited amount of servers. If different users on a single server
+# to a limited amount of servers. If different users on a single server
 # run jupyter notebook simultaneously, it may happen that the sessions
-# influence each other. To avoid this we need to make sure that each
-# user starts jupyther notebook with a different port the notebook
+# influence each other. To avoid this, we need to make sure that each
+# user starts his jupyter notebook with a different port that the notebook
 # listens to:
 
 # change the following line if you want to use another broswer than
 # firefox
 BROWSER=/usr/local/bin/firefox
 
+# The following line is specific to Heidelberg to acticate Anaconda
+# Python 3.7
+source /data/anaconda3/anaconda.sh
+
+# and start the notebook:
 jupyter notebook --port=$((`id -u` % 55535 + 10000)) --browser=${BROWSER}
